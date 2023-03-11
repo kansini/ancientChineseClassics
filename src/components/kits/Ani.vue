0df87fd43df0bbc8fc201034de1f3a184bf5d6b1
Old Flood <kansini@gmail.com>
2023-03-11 18:43:19 +0800

    fixed ts error

--- src/components/kits/Ani.vue
+++ src/components/kits/Ani.vue
@@ -1,11 +1,10 @@
 <template>
   <div :id="id"
-       :style="{width:width ,height:height,background:background}"
-       @click="toggleAni"></div>
+       :style="{width:width ,height:height,background:background}"></div>
 </template>
 
 <script lang="ts" setup>
-import * as lottie from "lottie-web"
+import lottie from "lottie-web"
 import {uuid} from "../../utils/uuid"
 import {ref, onMounted} from "vue"
 
@@ -37,37 +36,17 @@ const props = defineProps({
     }
   }
 })
-const isPlay = ref(true)
-const speed = ref(1)
 const id = uuid('ani-')
 const init = () => {
-  let cv = document.getElementById(id)
+  const cv: any = document.getElementById(id)
   lottie.loadAnimation({
     container: cv, // the dom element that will contain the animation
-    renderer: props.renderer,
+    renderer: 'svg',
     loop: true,
     autoplay: true,
-    path:  `./aniList/${props.name}.json` // props.path, // the path to the animation json
+    path: `./aniList/${props.name}.json` // props.path, // the path to the animation json
   })
   // lottie.setDirection(0);
-}
-const toggleSpeed = () => {
-  if (speed.value === 1) {
-    lottie.setSpeed(2)
-    speed.value = 2
-  } else {
-    lottie.setSpeed(1)
-    speed.value = 1
-  }
-}
-const toggleAni = () => {
-  // if (!isPlay.value) {
-  //   lottie.play()
-  //   isPlay.value = true
-  // } else {
-  //   lottie.pause()
-  //   isPlay.value = false
-  // }
 }
 onMounted(() => {
   init()
